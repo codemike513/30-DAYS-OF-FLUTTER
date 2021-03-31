1233b4ae9da3594985e1548fafc74ae3434d8c9f
Mihir Pesswani <pesswani.mihir@gmail.com>
2021-04-01 00:32:47 +0530

    Day 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,29 @@
-// DAY 1 OF FLUTTER -> Made a Basic First APP
+// DAY 2 OF FLUTTER -> Dart Data Types and Scaffold
 
 /*
--> Importing the Flutter Material Package
--> Main Method 
--> runApp Method to run our app and give our app as parameter
--> Creating a Stateless Widget
--> MAterialApp Widget to make our App
--> Material Widget to give a better UI to App
--> Center Widget to center our Widgets
--> Container Widget to make a Container
--> Text Widget to Display our Text
+DART DATA TYPES -
+
+-> Integer Data Type(Takes Integer Values) -> int days = 30;
+-> String Data Type(Takes Character or String Values) -> String name = "Mihir";
+-> Double Data Type(Takes floating point values) -> double b = 5.24;
+-> Boolean Data Type(Takes True or False) -> bool a = true;
+-> Num Data Type(Can be used for integer as well as for double values) -> num temp = 30; OR num tem = 30.5;
+-> Var Data Type(It's a Dynamic data type we don't have specify it's type it automatically generated it) -> var day = "Tuesday"; OR var day1 = 5;
+-> Constant Data Type(Takes Constant values and data cannot be modified) -> const pi = 3.14;
+-> Final Data Type(Same as constant but data can be modified)
+Difference btw final and const
+-> Suppose you have a List in final, so you can add to that List
+-> But Suppose you have a List in const, so you cannot add to that List
+
+String Interpolation -> Use $ symbol with our variable to extract its value and add to our Text.
+
+Let's Learn Now Scaffold Widget ->
+So we have made a new file as home_page.dart to use our widgets
+We have just given the HomePage() App to home of Material App and imported our home_page.dart file to use it
 */
 
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,15 +32,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Material(
-        child: Center(                                           
-          child: Container(
-            child: Text("Welcome to 30 days of Flutter"),
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(home: HomePage());
   }
 }
